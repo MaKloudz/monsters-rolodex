556b80e5710ee8711d53f98e62cb70660dc70f56
MaKloudz <39462957+MaKloudz@users.noreply.github.com>
2020-05-23 21:09:47 +0100

    Updates

---
+++ precache-manifest.a32dc8f4119fb22011b847695c7a0d4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb060e6d46251501db966af33716c4a5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6401e86b029953be3bbe",
+    "url": "/monsters-rolodex/static/css/main.3d287106.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6401e86b029953be3bbe",
+    "url": "/monsters-rolodex/static/js/main.b09a8451.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
